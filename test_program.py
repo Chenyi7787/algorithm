7f1fa42f57dd2dcae76ece2e21a49e2df181061e
bychen <bychen@sonicwall.com>
2021-01-05 15:24:34 +0800

    debug leetcode_242.py

---
+++ test_program.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+# author by Bruce Chen 2021/01/04
+'''
+This is a test program for daily
+'''
+
+from collections import Counter
+
+nums1 = [9,4,9,8,4]
+print(set(nums1), type(set(nums1)))
+
+nums1.sort()
+print(nums1)
+
+result1 = list()
+print(result1)
+result2 = []
+print(result2)
+# s = "anagram"
+#
+# t = "nagaram"
+#
+# print(sorted(s))
+# print(sorted(t))
+# print(sorted(""))
