f179ad1f437c2201e930354a9ef4e9c8a12b2322
bychen <bychen@sonicwall.com>
2021-01-07 18:20:01 +0800

    add leetcode_1528.py

--- test_program.py
+++ test_program.py
@@ -6,16 +6,7 @@ This is a test program for daily
 
 from collections import Counter
 
-nums1 = [9,4,9,8,4]
-print(set(nums1), type(set(nums1)))
 
-nums1.sort()
-print(nums1)
-
-result1 = list()
-print(result1)
-result2 = []
-print(result2)
 # s = "anagram"
 #
 # t = "nagaram"
