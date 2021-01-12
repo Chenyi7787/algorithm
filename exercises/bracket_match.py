8505d7f558bfc8598417bcb9a6954eb64ff51255
bychen <bychen@sonicwall.com>
2021-01-12 16:47:56 +0800

    finished bracket_mathch.py

--- exercises/bracket_match.py
+++ exercises/bracket_match.py
@@ -6,3 +6,40 @@
 # @File: bracket_match.py
 # @Software: PyCharm
 
+"""
+{[()]}
+"""
+import sys
+sys.path.append("../")
+from data_structure.stack import Stack
+
+class Solution:
+    def bracket_match(self, s):
+        """
+        :param s: str
+        :return: bool
+        """
+        dic = {')':'(', '}':'{', ']':'['}
+        stack = Stack()
+        for i in s:
+            if i in {'{', '[', '('}:
+                stack.push(i)
+            else:
+                if stack.get_top() == dic[i]:
+                    stack.pop()
+                else:
+                    return False
+        if stack.is_empty():
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    s = "{[()()]}"
+    test = Solution()
+    result = test.bracket_match(s)
+    print(result)
+
+
+
